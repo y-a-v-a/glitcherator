a871a2c3814d6cd46acad98504688308a11cf5ce
Vincent Bruijn <vebruijn@gmail.com>
2012-10-14 17:06:26 +0200

    reworked code now open and save and refresh

--- src/glitcherator/App.java
+++ src/glitcherator/App.java
@@ -1,20 +1,59 @@
 package glitcherator;
 
-import javax.swing.*;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class App {
 
 	private String imagename;
 
+	public static JFrame frame;
+	
 	public static void main(String[] args) {
 		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Glitcherator");
 		
 		App app = new App();
-		JFrame frame = new JFrame("Tutorials");
-		
 		app.imagename = args[0];
+		Glitcherator glitch = new Glitcherator(app.imagename);
 		
-		Glitcherator g = new Glitcherator(app.imagename);
-		g.build().show(frame).export("");
+		frame = new JFrame("Glitcherator");
+		frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		Dimension minimumSize = new Dimension(1000,800);
+		panel.setPreferredSize(minimumSize);
+		
+		JButton bttn = new JButton("Refresh");
+		bttn.setToolTipText("Refresh image");
+		bttn.addActionListener(glitch);
+		JButton bttn2 = new JButton("Save");
+		bttn2.addActionListener(glitch);
+		JButton bttn3 = new JButton("Open");
+		bttn3.addActionListener(glitch);
+		
+		panel.add(bttn);
+		panel.add(bttn2);
+		panel.add(bttn3);
+		frame.add(panel);
+		
+		StringBuffer title = new StringBuffer();
+		title.append(Glitcherator.VERSION);
+		title.append(" - ");
+		title.append(app.imagename);
+		title.append(" - y_a_v_a");
+		frame.setTitle(title.toString());
+				
+		
+		glitch.build();
+		
+		panel.add(glitch);
+		frame.setContentPane(panel);
+
+
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
--- src/glitcherator/Glitcherator.java
+++ src/glitcherator/Glitcherator.java
@@ -1,14 +1,29 @@
 package glitcherator;
 
-import java.awt.Graphics2D;
-import java.awt.image.*;
-import java.io.*;
-import java.util.*;
-import javax.swing.*;
-import javax.imageio.ImageIO;
+import java.awt.FileDialog;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
-public class Glitcherator {
-	private static final String VERSION = "Glitcherator 0.1";
+import javax.imageio.ImageIO;
+import javax.imageio.stream.FileImageInputStream;
+import javax.swing.JPanel;
+
+import org.apache.commons.codec.binary.Hex;
+
+public class Glitcherator extends JPanel implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+
+	static final String VERSION = "Glitcherator 0.1";
 	
 	private static final boolean DEBUG = true;
 	
@@ -21,6 +36,8 @@ public class Glitcherator {
 	private Map<String, String> debug = new HashMap<String, String>();
 	
 	private String filename;
+	
+	private byte[] imageRaw;
 
 	private Glitcherator() {
 		this.ctime = new Date().getTime();
@@ -31,52 +48,105 @@ public class Glitcherator {
 		this();
 		this.filename = filename;
 		this.debug.put("file", this.filename);
+		this.load();
+	}
+	
+	private void load() {
+		// first read original file
 		try {
-		    this.image = ImageIO.read(new File(this.filename));
+			this.image = ImageIO.read(new File(this.filename));
 		} catch (IOException e) {
 			System.out.println("Exception: " + e.getMessage());
 		}
-		doDebug();
+		
+		// then read it raw in bytes
+		try {
+			File f = new File(this.filename);
+			FileImageInputStream fis = new FileImageInputStream(f);
+			this.imageRaw = new byte[(int) f.length()];
+			fis.read(imageRaw);
+			
+		} catch (Exception e) {
+			System.out.println("io error");
+		}
+		
 	}
 	
 	public Glitcherator build() {
-		WritableRaster wr = this.image.getRaster();
-		double[] data = {2d, 3d, 4d };
-		int i = 0;
-		for (i = 0; i < 100; i++) {
-			wr.setPixel(i, i, data);
+		char[] myChar = Hex.encodeHex(imageRaw, true);
+		int max = myChar.length;
+		Long x = new Long(Math.round(Math.random() * max));
+		int k = Integer.parseInt(x.toString() );
+		
+		// stay within bounds
+		if ((k + 512) > max) {
+			k -= 512;
 		}
-		//wr.setPixel(100, 100, data);
+		
+		for(int i = k; i < (k + 512); i += 2) {
+			myChar[i] = '5';
+			myChar[i + 1] = 'e';
+		}
+		byte[] myArray = null;
+		
+		try {
+			myArray = Hex.decodeHex(myChar);
+		} catch (Exception e) {
+			System.out.println("decoding mishap");
+		}
 
-		java.util.Hashtable<String, String> properties = new java.util.Hashtable<String, String>();
-		properties.put("comment", "Created with Glitcherator");
-
-		ColorModel cm = this.image.getColorModel();
-		BufferedImage bi = new BufferedImage(cm, wr, false, properties);
-				
+		ByteArrayInputStream bais = new ByteArrayInputStream(myArray);
+		
+		BufferedImage bi = null;
+		try {
+			bi = ImageIO.read(bais);
+		} catch (IOException e) {
+			System.out.println("cannot read image?!");
+		}
 		this.def_img = bi;
 		return this;
+		
 	}
 	
-	public Glitcherator show(JFrame frame) {
-		StringBuffer title = new StringBuffer();
-		title.append(Glitcherator.VERSION);
-		title.append(" - ");
-		title.append(this.filename);
-		title.append(" - y_a_v_a");
-		frame.setTitle(title.toString());
+	/**
+	 * override from JPanel 
+	 */
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		
-		frame.setBounds(0, 0, this.def_img.getWidth(), this.def_img.getHeight());	
-		frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+		// center image on pane
+		Integer fromLeft = new Integer(0);
+		fromLeft = Integer.parseInt(String.valueOf(((1000 - this.getDefWidth()) / 2)));
+		this.setBounds(fromLeft, 40, this.getDefWidth(), this.getDefHeight());
 		
-		frame.setVisible(true);
-		Graphics2D g = (Graphics2D) frame.getRootPane().getGraphics();
-		g.drawImage(this.def_img, null, 0, 0);
-		g.dispose();
-		
-		frame.setLocationRelativeTo(null);
-		return this;
+		g.drawImage(this.def_img, 0, 0, null);
 	}
+	
+	public int getDefWidth() {
+		return this.image.getWidth();
+	}
+	
+	public int getDefHeight() {
+		return this.image.getHeight();
+	}
+	
+//	public Glitcherator show(JFrame frame) {
+//		frame.setBounds(0, 0, this.def_img.getWidth(), this.def_img.getHeight() + 100);
+//
+//		JPanel jp = new JPanel();
+//		jp.setBounds(0, 0, this.def_img.getWidth(), this.def_img.getHeight());
+//		jp.setVisible(true);
+//
+//		frame.add(jp);
+//		
+//		Graphics2D g = (Graphics2D) jp.getGraphics();
+//		g.drawImage(this.def_img, null, 0, 40);
+//		g.dispose();
+//		
+//		frame.repaint(10000);
+//
+//		return this;
+//	}
 	
 	/**
 	 * Export image to new file
@@ -85,8 +155,8 @@ public class Glitcherator {
 	 */
 	public Glitcherator export(String target) {
 		try {
-			String name = "img" + Long.toString(this.ctime) + ".jpg";
-		    File outputfile = new File(target + name);
+			//String name = "img" + Long.toString(this.ctime) + ".jpg";
+		    File outputfile = new File(target);//+ name);
 		    ImageIO.write(this.def_img, "jpg", outputfile);
 		} catch (IOException e) {
 		    System.out.println("Exception: " + e.getMessage());
@@ -125,6 +195,34 @@ public class Glitcherator {
 	 */
 	public String toString() {
 		return "Cannot convert image to string";
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand() == "Save") {
+			String file = "img" + Long.toString(this.ctime) + ".jpg";
+			FileDialog fd = new FileDialog(App.frame, null, FileDialog.SAVE);
+			fd.setTitle("Save Glitched file...");
+			fd.setAlwaysOnTop(true);
+			fd.setFile(file);
+			fd.setVisible(true);
+			String filename = fd.getDirectory() + fd.getFile();
+			this.export(filename);
+		}
+		if (e.getActionCommand() == "Refresh") {
+			this.ctime = new Date().getTime();
+			this.build();
+			this.repaint();
+		}
+		if(e.getActionCommand() == "Open") {
+			FileDialog fd = new FileDialog(App.frame, null, FileDialog.LOAD);
+			fd.setVisible(true);
+			String filename = fd.getDirectory() + fd.getFile();
+			this.filename = filename;
+			this.load();
+			this.build();
+			this.repaint();
+		}
 	}
 
 }
